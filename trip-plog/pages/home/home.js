f3998173a28aa673f892ccd0be13a8d11d585aaa
Laulx <15961230441@163.com>
2024-04-11 22:09:05 +0800

    个性签名

--- trip-plog/pages/home/home.js
+++ trip-plog/pages/home/home.js
@@ -23,7 +23,7 @@ Page({
 	},
 	// 跳转详情页
 	goToDetails(e) {
-		console.log("点击的日志id", e.currentTarget.dataset.id);
+		// console.log("点击的日志id", e.currentTarget.dataset.id);
 		wx.navigateTo({
 			url: '/pages/details/details?id=' + e.currentTarget.dataset.id,
 		})
@@ -31,41 +31,37 @@ Page({
 		// 先获取当前的 views 值
 		db.collection('article').doc(e.currentTarget.dataset.id).get().then(res => {
 			const currentViews = res.data.views;
-			console.log("currentViews", currentViews);
+			// console.log("当前浏览量", currentViews);
 			// 更新 views 字段
 			db.collection('article').doc(e.currentTarget.dataset.id).update({
 				data: {
 					views: currentViews + 1
 				},
-				success: res => {
-					console.log('views 字段更新成功', res);
-					// 获取更新后的数据
-					db.collection('article').get().then(res => {
-						// 更新新的 article 字段并获取
-						this.setData({
-							article: res.data
-						})
-						console.log('article 字段更新成功', this.data.article)
-					}).catch(error => {
-						console.error('获取更新后的数据失败', error);
-					});
-				},
-				fail: error => {
-					console.error('views 字段更新失败', error);
-				}
-			});
+			}).then(res => {
+				// 获取更新后的数据
+				db.collection('article').get().then(res => {
+					// 更新新的 article 字段并获取
+					this.setData({
+						article: res.data
+					})
+					// console.log('article 字段更新成功', this.data.article)
+				}).catch(error => {
+					console.error('获取更新后的数据失败', error);
+				});
+			})
 		}).catch(error => {
 			console.error('获取当前 views 值失败', error);
 		});
-  },
-    //跳转游记发布页
-    goToRecord: function() {
-      wx.navigateTo({
-        url: '/pages/record/record',
-        fail: function(err) {
-          console.error('跳转到record页失败', err);
-        }
-      });
-    },
+
+	},
+	//跳转游记发布页
+	goToRecord: function () {
+		wx.navigateTo({
+			url: '/pages/record/record',
+			fail: function (err) {
+				console.error('跳转到record页失败', err);
+			}
+		});
+	},
 
 })
